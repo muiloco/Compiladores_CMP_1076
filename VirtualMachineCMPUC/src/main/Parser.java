db88fd0f05b1397ca6f0d352edaa856e16f72236
muiloco <muiloco17@gmail.com>
2019-04-25 20:37:57 -0300

    Maquina Virtual parser alteration

--- VirtualMachineCMPUC/src/main/Parser.java
+++ VirtualMachineCMPUC/src/main/Parser.java
@@ -47,21 +47,115 @@ public class Parser {
                     nextToken();
                     if (this.token.getToken().equals("REGISTER")){
                         int destino = Integer.valueOf(Lexer.variaveis.get(this.token.getValor()));
+                        String registrador = this.token.getValor();
                         nextToken();
-                        if(this.token.getToken().equals("REGISTER")){
-                            int op1 = 
+                        if(this.token.getToken().equals("VIRG")){
+                            nextToken();
+                            operator2();
+                            int valor2 = (int) this.pilha.pop();
+                            int valor1 = (int) this.pilha.pop();
+                            destino = valor1 + valor2;
+                            Lexer.addHash(registrador, String.valueOf(destino));
                         }
+                    }
+                    else{
+                        throw new Exception("Erro 1 -- Registrador não identificado");
                     }
                     break;
                 case "SUB":
+                    nextToken();
+                    if (this.token.getToken().equals("REGISTER")){
+                        int destino = Integer.valueOf(Lexer.variaveis.get(this.token.getValor()));
+                        String registrador = this.token.getValor();
+                        nextToken();
+                        if(this.token.getToken().equals("VIRG")){
+                            nextToken();
+                            operator2();
+                            int valor2 = (int) this.pilha.pop();
+                            int valor1 = (int) this.pilha.pop();
+                            destino = valor1 - valor2;
+                            Lexer.addHash(registrador, String.valueOf(destino));
+                        }
+                    }
+                    else{
+                        throw new Exception("Erro 1 -- Registrador não identificado");
+                    }
                     break;
                 case "MUL":
+                    nextToken();
+                    if (this.token.getToken().equals("REGISTER")){
+                        int destino = Integer.valueOf(Lexer.variaveis.get(this.token.getValor()));
+                        String registrador = this.token.getValor();
+                        nextToken();
+                        if(this.token.getToken().equals("VIRG")){
+                            nextToken();
+                            operator2();
+                            int valor2 = (int) this.pilha.pop();
+                            int valor1 = (int) this.pilha.pop();
+                            destino = valor1 * valor2;
+                            Lexer.addHash(registrador, String.valueOf(destino));
+                        }
+                    }
+                    else{
+                        throw new Exception("Erro 1 -- Registrador não identificado");
+                    }
                     break;
                 case "DIV":
+                    nextToken();
+                    if (this.token.getToken().equals("REGISTER")){
+                        int destino = Integer.valueOf(Lexer.variaveis.get(this.token.getValor()));
+                        String registrador = this.token.getValor();
+                        nextToken();
+                        if(this.token.getToken().equals("VIRG")){
+                            nextToken();
+                            operator2();
+                            int valor2 = (int) this.pilha.pop();
+                            int valor1 = (int) this.pilha.pop();
+                            destino = valor1 / valor2;
+                            Lexer.addHash(registrador, String.valueOf(destino));
+                        }
+                    }
+                    else{
+                        throw new Exception("Erro 1 -- Registrador não identificado");
+                    }
                     break;
                 case "MOD":
+                    nextToken();
+                    if (this.token.getToken().equals("REGISTER")){
+                        int destino = Integer.valueOf(Lexer.variaveis.get(this.token.getValor()));
+                        String registrador = this.token.getValor();
+                        nextToken();
+                        if(this.token.getToken().equals("VIRG")){
+                            nextToken();
+                            operator2();
+                            int valor2 = (int) this.pilha.pop();
+                            int valor1 = (int) this.pilha.pop();
+                            destino = valor1 % valor2;
+                            Lexer.addHash(registrador, String.valueOf(destino));
+                        }
+                    }
+                    else{
+                        throw new Exception("Erro 1 -- Registrador não identificado");
+                    }
                     break;
                 case "POT":
+                    nextToken();
+                    if (this.token.getToken().equals("REGISTER")){
+                        int destino = Integer.valueOf(Lexer.variaveis.get(this.token.getValor()));
+                        String registrador = this.token.getValor();
+                        nextToken();
+                        if(this.token.getToken().equals("VIRG")){
+                            nextToken();
+                            operator2();
+                            int valor2 = (int) this.pilha.pop();
+                            int valor1 = (int) this.pilha.pop();
+                            destino = (int) Math.pow(valor1, valor2);
+                            Lexer.addHash(registrador, String.valueOf(destino));
+                        }
+                    }
+                    else{
+                        throw new Exception("Erro 1 -- Registrador não identificado");
+                    }
                     break;
                 case "AND":
                     break;
@@ -98,6 +192,39 @@ public class Parser {
                 default:
                     throw new Exception("Deu Pau na Maquina, comando não identificado");
             }
+        }
+    }
+    
+    void operator2() throws Exception{
+        if(this.token.getToken().equals("REGISTER")){
+            int valor1 = Integer.valueOf(Lexer.variaveis.get(this.token.getValor()));
+            this.pilha.push(valor1);
+            nextToken();
+            if(this.token.getToken().equals("VIRG")){
+                nextToken();
+                operator3();
+            }
+        } else {
+            throw new Exception("Erro 2 -- Erro no segundo registrador");
+        }
+    }
+    
+    void operator3() throws Exception{
+        switch (this.token.getToken()) {
+            case "REGISTER":
+                {
+                    int valor2 = Integer.valueOf(Lexer.variaveis.get(this.token.getValor()));
+                    this.pilha.push(valor2);
+                    break;
+                }
+            case "NUM":
+                {
+                    int valor2 = Integer.valueOf(this.token.getValor());
+                    this.pilha.push(valor2);
+                    break;
+                }
+            default:
+                throw new Exception("Erro 3 -- Erro no terceiro registrador do comando");
         }
     }
 
